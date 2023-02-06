7caabb43b36b20c4b908d8ae59dbc54439d9d998
Kirill Voronin <yoursdearboy@gmail.com>
2023-02-06 11:38:19 +0300

    Simplified reading EDN

--- test/boss/query_test.clj
+++ test/boss/query_test.clj
@@ -1,7 +1,6 @@
 (ns boss.query-test
   (:require
     [clojure.edn :as edn]
-    [clojure.java.io :as io]
     [next.jdbc :as jdbc]
     [honey.sql :as sql]
     [boss.pivot :refer [pivot-query]]
@@ -11,16 +10,9 @@
 (def db {:dbtype "sqlite" :dbname "test/demo.db"})
 (def ds (jdbc/get-datasource db))
 
-(defn load-edn
-  "Load edn from an io/reader source (filename or io/resource)."
-  [source]
-  (try
-    (with-open [r (io/reader source)]
-      (edn/read (java.io.PushbackReader. r)))
-    (catch java.io.IOException e
-      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
-    (catch RuntimeException e
-      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
+(defn load-edn [source]
+  "Load edn from file"
+  (edn/read-string (slurp source)))
 
 ; FIXME: Test structures and/or queries and/or data?
 (deftest test-select-from
